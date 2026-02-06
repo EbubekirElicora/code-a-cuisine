6232abd6bb6b869f4a47559ce5d052a6dd724a8d
EbubekirElicora <ebubekir.elicora@gmx.de>
2026-02-06 21:29:27 +0100

    new

--- src/app/core/recipes/recipe.endpoints.ts
+++ src/app/core/recipes/recipe.endpoints.ts
@@ -1,5 +1,14 @@
-export const RECIPE_ENDPOINTS = {
+/* export const RECIPE_ENDPOINTS = {
   recipes: 'http://localhost:5678/webhook/recipes',
   generate: 'http://localhost:5678/webhook/generate-recipes',
   like: 'http://localhost:5678/webhook/like-recipe',
-} as const;
+} as const; */
+
+
+export const API_BASE = 'https://ebubekir-elicora.app.n8n.cloud';
+
+export const RECIPE_ENDPOINTS = {
+  recipes: `${API_BASE}/webhook/recipes`,
+  generate: `${API_BASE}/webhook/generate-recipes`,
+  like: `${API_BASE}/webhook/like-recipe`,
+};
